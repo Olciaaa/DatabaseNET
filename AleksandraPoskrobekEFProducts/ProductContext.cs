d7b3b2433ca8fba4cea1db9c96687cfadae92be7
poskrobe <ola.poskrobek@gmail.com>
2023-04-05 16:03:06 +0200

    initialization

---
+++ AleksandraPoskrobekEFProducts/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleksandraPoskrobekEFProducts
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Datasource=ProductDatabase");
+        }
+    }
+}
---
+++ AleksandraPoskrobekEFProducts/Program.cs
@@ -0,0 +1,12 @@
+﻿using AleksandraPoskrobekEFProducts;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+            ProductContext productContext = new ProductContext();
+            Product product = new Product { ProductName = "Flamaster" };
+            productContext.Products.Add(product);
+            productContext.SaveChanges();
+    }
+}
